3cd1a7ef0697d3c209068a4ffecabf55fa7e5f30
Victor804 <victor.vaucel@gmail.com>
2022-03-14 15:04:22 +0100

    update

--- olymines/urls.py
+++ olymines/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('interne', views.interne, name="activite interne"),
     path('externe', views.externe, name="activite externe"),
-    path('boutique', views.boutique, name="boutique"),
-    path('calendar', views.calendrier, name="calendrier")
-
+    path('presentation', views.presentation, name="presentation")
 ]
--- olymines/views.py
+++ olymines/views.py
@@ -4,14 +4,11 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
+def presentation(request):
+    return render(request, "presentation.html")
+
 def interne(request):
     return HttpResponse("Activité interne")
 
 def externe(request):
     return HttpResponse("Activité externe")
-
-def boutique(request):
-    return HttpResponse("Boutique")
-
-def calendrier(request):
-    return render(request, 'calendar.html')
