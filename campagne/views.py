0a93541e7420089779f934ef7254b92e4bea6fce
Victor804 <victor.vaucel@gmail.com>
2022-03-17 12:32:31 +0100

    update

--- campagne/views.py
+++ campagne/views.py
@@ -7,8 +7,13 @@ def index(request):
 
 
 def chart(request):
-    return render(request, "chart.html", {"bleu":Team.objects.filter(color="bleu").get().score,
-                                          "jaune":Team.objects.filter(color="jaune").get().score,
-                                          "violet":Team.objects.filter(color="violet").get().score,
-                                          "vert":Team.objects.filter(color="vert").get().score,
-                                          "rouge":Team.objects.filter(color="rouge").get().score})
+    return render(request, "chart.html", {"equipe1":Team.objects.filter(color="equipe1").get().score,
+                                          "equipe2":Team.objects.filter(color="equipe2").get().score,
+                                          "equipe3":Team.objects.filter(color="equipe3").get().score,
+                                          "equipe4":Team.objects.filter(color="equipe4").get().score,
+                                          "equipe5":Team.objects.filter(color="equipe5").get().score,
+                                          "equipe6":Team.objects.filter(color="equipe6").get().score,
+                                          "equipe7":Team.objects.filter(color="equipe7").get().score,
+                                          "equipe8":Team.objects.filter(color="equipe8").get().score,
+                                          "equipe9":Team.objects.filter(color="equipe9").get().score,
+                                          "equipe10":Team.objects.filter(color="equipe10").get().score})
